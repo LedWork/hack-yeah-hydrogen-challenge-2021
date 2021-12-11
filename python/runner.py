bf634e1d00a0d0c231422eaf882682620f5f7b46
Jakub Ledworowski <jakub.ledworowski@gmail.com>
2021-12-11 14:03:42 +0100

    First problem

--- python/runner.py
+++ python/runner.py
@@ -1,6 +1,7 @@
 """Naval Fate.
 
 Usage:
+  runner.py
   runner.py (-h | --help)
   runner.py --version
 
@@ -10,8 +11,24 @@ Options:
 
 """
 from docopt import docopt
+import pulp as plp
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Runner 1.0')
     print(arguments)
+
+    opt_model = plp.LpProblem(name="MIP Model")
+
+    x = plp.LpVariable("x", 0, 3)
+    y = plp.LpVariable("y", 0, 1)
+    prob = plp.LpProblem("myProblem", plp.LpMinimize)
+    prob += x + y <= 2
+    prob += -4 * x + y
+
+    status = prob.solve()
+    # status = prob.solve(GLPK(msg=0))
+
+    print("status = " + plp.LpStatus[status])
+    print("x = " + str(plp.value(x)))
+    print("y = " + str(plp.value(y)))
